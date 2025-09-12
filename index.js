3095f631eba56549c1c84336e84a07af91133e77
Wandile Langa <langamitch69@gmail.com>
2025-09-12 19:54:32 +0200

    updated ui

--- index.js
+++ index.js
@@ -316,7 +316,10 @@ function renderApis(items){
       <div class="menu-toggle"> <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff"><path d="M240-400q-33 0-56.5-23.5T160-480q0-33 23.5-56.5T240-560q33 0 56.5 23.5T320-480q0 33-23.5 56.5T240-400Zm240 0q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm240 0q-33 0-56.5-23.5T640-480q0-33 23.5-56.5T720-560q33 0 56.5 23.5T800-480q0 33-23.5 56.5T720-400Z"/></svg>
       </div>
       </div>
-      <p>${it.description || ''}</p>
+      <p style="font-size: 14px;
+    margin-top: 10px;
+    margin-bottom: 10px; font-weight:300;
+">${it.description || ''}</p>
       <div class="api-row">
         <span class="api-key" title="${it.key}">${masked}</span>
       </div>
